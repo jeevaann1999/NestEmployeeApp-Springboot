b5ae67925605061f01647ee504ce9085c2389b0e
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-12 05:13:47 +0530

    add task

---
+++ src/main/java/com/example/nestemployeeapp_backend/model/Task.java
@@ -0,0 +1,62 @@
+package com.example.nestemployeeapp_backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "task")
+public class Task {
+
+    @Id
+    @GeneratedValue
+    private int id;
+    private int empcode;
+    private String task;
+    private String status;
+
+    public Task() {
+    }
+
+    public Task(int id, int empcode, String task, String status) {
+        this.id = id;
+        this.empcode = empcode;
+        this.task = task;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getEmpcode() {
+        return empcode;
+    }
+
+    public void setEmpcode(int empcode) {
+        this.empcode = empcode;
+    }
+
+    public String getTask() {
+        return task;
+    }
+
+    public void setTask(String task) {
+        this.task = task;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
+
+
